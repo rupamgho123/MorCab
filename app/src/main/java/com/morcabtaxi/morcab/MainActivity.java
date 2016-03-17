c7928578bbbc874141093f4a762b8f8cfc63cf7e
RupamGhosh <rupam.ghosh@flipkart.com>
2016-03-17 21:54:00 +0530

    release config

--- app/src/main/java/com/morcabtaxi/morcab/MainActivity.java
+++ app/src/main/java/com/morcabtaxi/morcab/MainActivity.java
@@ -24,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
 
   private void initAdMob(){
     AdView mAdView = (AdView) findViewById(R.id.adView);
-    AdRequest adRequest = new AdRequest.Builder().build();
+    AdRequest adRequest = new AdRequest.Builder()
+        .setIsDesignedForFamilies(true)
+        .build();
     mAdView.loadAd(adRequest);
   }
 }
